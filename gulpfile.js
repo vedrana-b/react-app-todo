adda3a1b4a764a843b63480b8c1e21d843471c1c
vedrana-b <vedrana93@gmail.com>
2020-05-26 22:28:30 +0200

    fix mobile screen

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,5 @@ gulp.task("sass", function () {
   return gulp
     .src("./src/App.scss")
     .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest("./src/App.css"));
+    .pipe(gulp.dest("./src/Css"));
 });
